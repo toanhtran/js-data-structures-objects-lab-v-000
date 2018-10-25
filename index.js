6db604a359bbc4b8495f9f93772c910f8d3b53f4
ToAnh Tran <toanh.t.tran@gmail.com>
2018-10-25 22:44:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const driver = {};
 function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, { [key]: value });
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+}
